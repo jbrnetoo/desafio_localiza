ea149371f960263110c65dbf254816ad1e991969
João Neto <joao.neto96@souunit.com.br>
2021-08-20 22:05:29 -0300

    Criação do controller faturação e implementação do swagger

---
+++ src/Api_Fatoracao/Controllers/FaturacaoController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+
+namespace Api_Fatoracao.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FaturacaoController : ControllerBase
+    {
+
+        #region Decompor Números
+        /// <summary>
+        /// Decompõe um número em todos os seus divisores
+        /// </summary>
+        /// <param name="numero">Número a ser fatorado</param>
+        /// <returns>
+        /// 200 - Operação realizada!
+        /// </returns>
+        [HttpGet("")]
+        public ActionResult DecomporNumeros(int numero)
+        {
+            try
+            {
+                return Ok(new { Msg = "Hello World"});
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
+        }
+        #endregion
+    }
+}
--- src/Api_Fatoracao/Startup.cs
+++ src/Api_Fatoracao/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Api_Fatoracao.Config;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Api_Fatoracao
 {
@@ -22,19 +16,33 @@ namespace Api_Fatoracao
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddRouting();
+
+            services.AddCors();
+
+            SwaggerConfig.Register(services);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Documentação");
+                c.InjectStylesheet("/swagger/theme-flattop.css");
+            });
 
             app.UseHttpsRedirection();
 
