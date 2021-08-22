26e18308c52a931aa31f6d973726172731194b57
João Neto <joao.neto96@souunit.com.br>
2021-08-22 07:25:03 -0300

    Separação dos métodos para outra classe

--- src/Api_Fatoracao/Controllers/FaturacaoController.cs
+++ src/Api_Fatoracao/Controllers/FaturacaoController.cs
@@ -1,9 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Api_Fatoracao.Utils;
+using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
 
 namespace Api_Fatoracao.Controllers
 {
@@ -26,7 +24,7 @@ namespace Api_Fatoracao.Controllers
         {
             try
             {
-                var resultado = MontarTexto(numero);
+                var resultado = Operacoes.MontarTexto(numero);
 
                 return Ok(resultado.ToString());
             }
@@ -34,65 +32,6 @@ namespace Api_Fatoracao.Controllers
             {
                 return StatusCode((int)HttpStatusCode.InternalServerError);
             }
-        }
-        #endregion
-
-        #region Métodos Privados
-        private StringBuilder MontarTexto(int numero)
-        {
-            StringBuilder sb = new StringBuilder();
-
-            sb.Append($"Número de entrada: {numero}");
-
-            sb.Append("\nNúmeros divisores: ");
-
-            foreach (var divisor in ObterDivisores(numero))
-            {
-                sb.Append($"{divisor} ");
-            }
-
-            sb.Append("\nDivisores primos: ");
-
-            foreach (var numeroPrimo in ObterPrimosDeEspecificoNumero(numero))
-            {
-                sb.Append($"{numeroPrimo} ");
-            }
-
-            return sb;
-        }
-
-        private List<int> ObterDivisores(int numero)
-        {
-            List<int> listaDivisores = new List<int>();
-
-            for (int i = 1; i <= numero; i++)
-                if (numero % i == 0)
-                    listaDivisores.Add(i);
-            return listaDivisores;
-        }
-
-        private List<int> ObterPrimosDeEspecificoNumero(int numero)
-        {
-            var listaNumerosPrimos = new List<int>();
-            for (var i = 2; i <= numero + 1; i++)
-            {
-                var flAdicionar = true;
-                foreach (var primo in listaNumerosPrimos)
-                {
-                    if (primo * primo > i)
-                        break;
-                    if (i % primo == 0)
-                    {
-                        flAdicionar = false;
-                        break;
-                    }
-                }
-                if (flAdicionar)
-                    listaNumerosPrimos.Add(i);
-            }
-            listaNumerosPrimos.Insert(0, 1);
-
-            return listaNumerosPrimos.Where(x => numero % x == 0).ToList();
         }
         #endregion
     }
